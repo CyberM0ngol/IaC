cc2dca467a8bb5706894ec35663ef626118e7419
sysninja <41381728+sysninja@users.noreply.github.com>
2018-07-18 16:02:53 -0400

    Created Deprovision-lab-textual-runbook.ps1 in AzureAutomation.

---
+++ Deprovision-lab-textual-runbook.ps1
@@ -0,0 +1,18 @@
+<# 
+	This PowerShell script was automatically converted to PowerShell Workflow so it can be run as a runbook.
+	Specific changes that have been made are marked with a comment starting with “Converter:”
+#>
+workflow Deprovision-lab-textual-runbook {
+	
+	# Converter: Wrapping initial script in an InlineScript activity, and passing any parameters for use within the InlineScript
+	# Converter: If you want this InlineScript to execute on another host rather than the Automation worker, simply add some combination of -PSComputerName, -PSCredential, -PSConnectionURI, or other workflow common parameters (http://technet.microsoft.com/en-us/library/jj129719.aspx) as parameters of the InlineScript
+	inlineScript {
+		 $c = Get-AutomationConnection -Name 'AzureRunAsConnection'  
+		
+ 		Add-AzureRmAccount -ServicePrincipal -Tenant $c.TenantID -ApplicationID $c.ApplicationID -CertificateThumbprint $c.CertificateThumbprint 
+		
+ 		$resourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'  
+		
+ 		Remove-AzureRmResourceGroup -Name $resourceGroupName -Force
+	}
+}
